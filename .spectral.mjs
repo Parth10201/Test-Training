1de44e5d1054e7ca12910b612b45d37b65e7578d
Parth Kapadia <yash.gohel@rapidops.com>
2023-02-20 16:31:38 +0530

    Added 5 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NTU4NQ/spectral.js?branch_slug=master&token=217656e9-83e6-4e25-a86c-42c3dec9a4ca";
+export default { extends: ruleset };
